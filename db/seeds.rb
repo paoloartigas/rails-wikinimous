138c60288df09e252ec04141294f9f2bbd2f8221
Paolo Lenin Artigas Chacin <paoloartigas21@gmail.com>
2025-05-17 21:23:07 -0400

    Wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+
+10.times do
+  Article.create!(title: Faker::Lorem.sentence,
+    content: Faker::Lorem.paragraph)
+end
